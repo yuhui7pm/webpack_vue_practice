2791a65a25cb6ab7afa10c41f61edb83b911293f
yuhui1998 <1176951680@qq.com>
2019-12-04 16:17:34 +0800

    将静态资源(css)与js分开来打包

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HTMLPlugin = require('html-webpack-plugin');
 // import webpack from "webpack";   //import是ES6的语法，加载方式是编译时运行
 const webpack = require("webpack"); //require是CommonJs/AMD的语法，加载方式是运行时加载
+const extractTextPlugin = require("extract-text-webpack-plugin");
 
 const isDev = process.env.NODE_ENV==="development";
 // console.log('isDev:',isDev)
@@ -11,7 +12,7 @@ const config = {
   target:'web',//webpack编译目标是web平台
   entry:path.join(__dirname,'src/index.js'),
   output:{
-    filename:"bundle.js",
+    filename:"bundle.[hash:8].js",
     path:path.join(__dirname,'dist'),
   },
   //webpack默认只支持js，所以对.vue文件要使用vue-loader,输出正确的js代码
@@ -43,22 +44,6 @@ const config = {
           }
         }
       },
-      {
-        test:/\.styl/,
-        use:[
-          "style-loader",
-          "css-loader",
-          {
-            //自动添加浏览器前缀 
-            loader:"postcss-loader",
-            //使用stylus-loader已经生成的sourceMap
-            options:{
-              sourceMap:true
-            }
-          },
-          "stylus-loader"
-        ]
-      }
     ]
   },
   plugins:[
@@ -79,6 +64,24 @@ const config = {
 }
 //如果是开发模式，就调用webpack-dev-server
 if(isDev){
+  config.module.rules.push(
+    {
+      test:/\.styl/,
+      use:[
+        "style-loader",
+        "css-loader",
+        {
+          //自动添加浏览器前缀 
+          loader:"postcss-loader",
+          //使用stylus-loader已经生成的sourceMap
+          options:{
+            sourceMap:true
+          }
+        },
+        "stylus-loader"
+      ]
+    }
+  )
   //devtool配置的定义很简单：选择一种 source map 格式来增强调试过程，
   //不同的值会明显影响到构建build和重新构建rebuild的速度。
   //cheap-module-eval-source-map：原始源代码（仅限行）
@@ -101,6 +104,37 @@ if(isDev){
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(), //热加载插件
     new webpack.NoEmitOnErrorsPlugin() //启用此插件后，webpack 进程遇到错误代码将不会退出。
+  )
+}else{
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test:/\.styl/,
+      //如果是生产模式就将静态资源文件单独打包出来
+      use:extractTextPlugin.extract({
+/*         css-loader 的作用是处理css文件中 @import，url之类的语句，style-loader
+        则是将css文件内容放在style标签内并插入head中，既然使用了extract-text-webpack-plugin，
+        按理说就没必要配置style-loader了，实际上你不配置也可以，可是如果你不想分离css文件，
+        可设置disable:ture来关闭该插件，这时fallback:'style-loader'就派上用场了 */
+        fallback:'style-loader',
+        use:[
+          "css-loader",
+          {
+            //自动添加浏览器前缀 
+            loader:"postcss-loader",
+            //使用stylus-loader已经生成的sourceMap
+            options:{
+              sourceMap:true
+            }
+          },
+          "stylus-loader"
+        ]
+      })
+    }
+  )
+  //contenthash是针对文件内容级别的，只有你自己模块的内容变了，那么hash值才改变，
+  config.plugins.push(
+    new extractTextPlugin('styles.[md5:contenthash:hex:8].css') //因为webpack4.3包含了contenthash这个字段，所以不能使用contenthash，要使用md5:contenthash:hex:8来替代
   )
 }
 
